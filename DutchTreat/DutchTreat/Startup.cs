596dd92da43779ab4172c26e157a42313cceadb1
DevJHennessy <34386510+DevJHennessy@users.noreply.github.com>
2018-11-27 10:29:29 -0800

    Adding JS and jQuery functionality.

---
+++ DutchTreat/DutchTreat/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DutchTreat
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            //Configuring the web server to do something:
+            //Using static files only serves files inside the wwwroot directory.
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseNodeModules(env);
+
+
+
+            //Default code:
+            
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //A Lamda expression that says anytime a request comes in, write out Hello
+            //World.
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
